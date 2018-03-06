c074b61256b27a5b71f76c7e641c361e3c9ee89d
Christian Lazo <lazochristian.cl@gmail.com>
2018-03-06 13:31:40 -0800

    Add button

---
+++ random_quote_machine/javascript.js
@@ -0,0 +1,14 @@
+function getQuote(){
+  $.ajax({
+    url: '/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+    success: function(data) {
+      var post = data.shift();
+      $('#quote-content').html(post.content);
+    }
+  })
+}
+
+$(document).ready(function(){
+  getQuote();
+  $('#new-quote').on('click', getQuote);
+})
