e1d14085be93cb3c46de22b22c8494e2a3b24b9a
Christian Lazo <lazochristian.cl@gmail.com>
2018-03-09 11:58:27 -0800

    WIP: Add js to pull random quote

--- random_quote_machine/js/javascript.js
+++ random_quote_machine/js/javascript.js
@@ -1,11 +1,24 @@
-function getQuote(){
+var currentQuote = "", currentAuthor = "";
+
+function getQuote() {
   $.ajax({
-    url: '/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
-    success: function(data) {
-      var post = data.shift();
-      $('#quote-content').html(post.content);
+    headers: {
+      "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
+      Accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
+    success: function(r) {
+      if (typeof r === 'string') {
+       r = JSON.parse(r);
+      }
+      currentQuote = r.quote;
+      currentAuthor = r.author;
+
+      $('#quote').text(r.quote);
+      $('#author').text(r.author);
     }
-  })
+  });
 }
 
 $(document).ready(function(){
